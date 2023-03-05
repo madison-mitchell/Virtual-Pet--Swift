e9bcca0b20defe2efe8f5ce970f657af25ab133f
Madison Mitchell <madisonrosemitchell@gmail.com>
2023-03-04 23:17:10 -0500

    Initial Project

---
+++ VirtualPet/VirtualPet.swift
@@ -0,0 +1,71 @@
+//
+//  VirtualPet.swift
+//  VirtualPet
+//
+//  Created by Madison Mitchell on 3/4/23.
+//
+
+import Foundation
+class VirtualPet {
+    var name: String
+    var health = 100
+    var hunger = 60
+    var thirst = 65
+    var energy = 55
+    var mood = "Bored"
+    var isAlive = true
+    
+    init(name: String, health: Int = 100, hunger: Int = 60, thirst: Int = 65, energy: Int = 55, mood: String = "Bored", isAlive: Bool = true) {
+        self.name = name
+        self.health = health
+        self.hunger = hunger
+        self.thirst = thirst
+        self.energy = energy
+        self.mood = mood
+        self.isAlive = isAlive
+    }
+    
+    func feed() {
+        hunger += 40
+        thirst -= 9
+        
+        if hunger > 60 {
+            hunger = 100
+        } else if hunger < 0 {
+            hunger = 0
+            health -= 10
+        }
+    }
+    
+    func water() {
+        thirst += 31
+        
+        if thirst > 69 {
+            thirst = 100
+        } else if thirst < 0 {
+            thirst = 0
+            health -= 15
+        }
+    }
+    
+    func play(activity: Int) {
+        if activity == 1 {
+            mood = "Playful"
+            energy -= 20
+        } else if activity == 2 {
+            mood = "Confused"
+            energy -= 1
+        }
+    }
+        
+    func tick() {
+        health -= 7
+        hunger -= 9
+        thirst -= 13
+        energy -= 11
+        
+        mood = energy <= 30 ? "Bored" : "Sleepy"
+        health -= energy <= 0 ? 5 : 0
+        isAlive = health <= 0 ? false : true
+    }
+}
---
+++ VirtualPet/main.swift
@@ -0,0 +1,57 @@
+//
+//  main.swift
+//  VirtualPet
+//
+//  Created by Madison Mitchell on 3/4/23.
+//
+
+import Foundation
+
+print("Welcome to Virtual Pet!")
+print("What would you like to name your pet?")
+var petName: String = readLine()!
+
+var pet = VirtualPet(name: petName)
+
+print("\n\(pet.name) loves you already! *wag wag wag*")
+
+while pet.isAlive {
+    print("_____________________________________________________________")
+    print("| Health\t| Hunger\t| Thirst\t| Energy\t| Mood\t\t|")
+    print("| \(pet.health)\t\t| \(pet.hunger)\t\t| \(pet.thirst)\t\t| \(pet.energy)\t\t| \(pet.mood)\t\t|")
+    print("_____________________________________________________________")
+
+    print("\nHow would you like to interact with \(pet.name)?")
+    print("1.\tFeed")
+    print("2.\tWater")
+    print("3.\tPlay")
+    let userChoice: Int = Int(readLine()!)!
+    
+    if userChoice == 1 {
+        pet.feed()
+        print("\n*nom nom nom...")
+    }
+    if userChoice == 2 {
+        pet.water()
+        print("\n*slurp slurpp sluuurpppp!")
+    }
+    if userChoice == 3 {
+        print("\nWhat would you like to play?")
+        print("1.\tFrisbee")
+        print("2.\tCards")
+        let userChoice = Int(readLine()!)!
+        
+        if userChoice == 1 {
+            pet.play(activity: 1)
+            print("\(pet.name) is feeling \(pet.mood.lowercased())!")
+        } else if userChoice == 2 {
+            pet.play(activity: 2)
+            print("\(pet.name) is rather... \(pet.mood).)")
+        }
+        
+    }
+    pet.tick()
+}
+
+print("Oh no! \(pet.name) fainted!")
+print("\nThank you for playing Virtual Pet!\n\n\n")
